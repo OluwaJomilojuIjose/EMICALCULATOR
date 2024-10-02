e9d8326f68106cf1a329b8d0f4bef0b403e4f3a4
Samuel Ijose <117550541+D4rkChocol4te@users.noreply.github.com>
2024-10-02 15:55:26 -0400

    uploaded assignment

---
+++ EMICalculator/app/src/main/java/com/example/emicalculator/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.emicalculator;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText mortgageAmount = findViewById(R.id.etMortgageAmount);
+        EditText interestRate = findViewById(R.id.etInterestRate);
+        EditText tenure = findViewById(R.id.etTenure);
+        Button calculateButton = findViewById(R.id.btnCalculate);
+        TextView result = findViewById(R.id.tvResult);
+
+        calculateButton.setOnClickListener(v -> {
+            double P = Double.parseDouble(mortgageAmount.getText().toString());
+            double annualRate = Double.parseDouble(interestRate.getText().toString());
+            int n = Integer.parseInt(tenure.getText().toString());
+
+            double r = (annualRate / 100) / 12;
+            double EMI = (P * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
+
+            result.setText("Your monthly EMI is: " + String.format("%.2f", EMI));
+        });
+    }
+}
